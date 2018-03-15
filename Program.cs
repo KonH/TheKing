4c5d1c57806119190e81fe514ab8f6a78fb145a6
KonH <konh@yandex.ru>
2018-03-16 01:39:26 +0700

    Localization sample setup;

--- Program.cs
+++ Program.cs
@@ -6,7 +6,8 @@ namespace TheKing
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+			//Content.Culture = new System.Globalization.CultureInfo("ru-RU");
+			Console.WriteLine(Content.hello);
         }
     }
 }
