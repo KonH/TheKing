926e92e895573469b8ab74f52cc6f293f3ba6c6e
KonH <konh@yandex.ru>
2018-03-17 01:31:23 +0700

    Simple population growth;

--- Controllers/PopulationController.cs
+++ Controllers/PopulationController.cs
@@ -2,14 +2,22 @@
 
 namespace TheKing.Controllers {
 	class PopulationController : StateController, INextDayHandler {
-		public int    Population { get; } = 100;
+		public int    Population { get; set; } = 100;
 		public double TaxRate    { get; } = 0.25;
+		public double GrowthRate { get; } = 0.01;
+
+		double _growthAccum;
 
 		public PopulationController(GameState state) : base(state) { }
 
 		public void OnNextDay() {
 			var taxCount = (int)Math.Round(Population * TaxRate);
-			State.Money.Add(Content.taxes_name, new Gold(taxCount));
+			State.Money.Add($"{Content.taxes_name} ({Population})", new Gold(taxCount));
+			_growthAccum += Population * GrowthRate;
+			while ( _growthAccum > 1 ) {
+				_growthAccum--;
+				Population++;
+			}
 		}
 	}
 }
